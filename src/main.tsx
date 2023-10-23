31102ae15a73a3541d430966a47bea4938a65122
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-23 22:53:44 +0700

    feat: index route

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import OldWebsite from "./routes/old";
 
 import "./styles/tailwind.css";
+import IndexRoute from "./routes/index";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/old",
-    element: <OldWebsite></OldWebsite>,
-  },
+  { path: "/", element: <IndexRoute /> },
+  { path: "/old", element: <OldWebsite /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+export default function IndexRoute() {
+  return (
+    <div>
+      <nav>Navigation</nav>
+
+      <main>
+        <section>
+          <h1>Hello, I'm Jonathan Nicolas, a web developer Contact Me</h1>
+        </section>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
