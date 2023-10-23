92fcef893595a0f7921a932b199ce3af37422dd6
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-23 23:15:44 +0700

    feat: section hero

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,8 +13,8 @@ export default function IndexRoute() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <nav className="flex justify-between gap-2 p-4 shadow-md">
-        <span className="text-2xl">Jonathan Nicolas</span>
+      <nav className="flex flex-wrap justify-between gap-2 p-4 shadow-md">
+        <span className="text-2xl font-bold">Jonathan Nicolas</span>
 
         <ul className="flex gap-8 items-center">
           {navigationItems.map((navItem) => {
@@ -33,8 +33,29 @@ export default function IndexRoute() {
       </nav>
 
       <main className="flex-[1]">
-        <section id="section-hero">
-          <h1>Hello, I'm Jonathan Nicolas, a web developer Contact Me</h1>
+        <section className="justify-center flex flex-wrap gap-10 bg-indigo-500 py-20 px-10">
+          <div className="hero-person flex flex-col gap-8 items-center">
+            <img
+              src="/images/avatar.jpg"
+              alt="Avatar"
+              className="rounded-full"
+              width={300}
+              height={300}
+            />
+            <h1 className="text-2xl font-semibold max-w-sm text-center text-indigo-100">
+              Hello, I'm Jonathan Nicolas, a web developer Contact Me
+            </h1>
+          </div>
+
+          <div>
+            <img
+              src="/images/hero-image.svg"
+              alt="Hero Image"
+              className="max-w-md object-contain"
+              width={500}
+              height={30}
+            />
+          </div>
         </section>
 
         <section id="section-other">
