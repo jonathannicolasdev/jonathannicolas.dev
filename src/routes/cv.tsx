bd2a3827e511d127409a677a3a28f3cb1c996add
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-10-26 12:32:59 -0400

    feat: cv about me card

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react";
+
 export default function CVRoute() {
   return (
     <div className="bg-slate-200 min-h-screen">
@@ -18,8 +20,28 @@ export default function CVRoute() {
           </section>
 
           <section className="col-span-1 md:col-span-2">
-            <div className="card">
-              <h1>About me</h1>
+            <div className="card space-y-4">
+              <h1 className="card-title">About me</h1>
+              <p>
+                Libero quas veritatis nulla distinctio fuga nihil temporibus et.
+                Quia dicta sapiente qui porro molestiae nobis incidunt
+                voluptatem. Et voluptas sunt nihil. At perferendis voluptatem
+                dolores nulla. Adipisci dolore non. Praesentium ipsa magnam ut
+                quia explicabo voluptates.
+              </p>
+              <p>
+                <a
+                  href="mailto:jonathannicolas.dev@gmail.com"
+                  className="text-green-800 font-semibold"
+                >
+                  jonathannicolas.dev@gmail.com
+                </a>
+              </p>
+              <div className="flex gap-4">
+                <Icon icon="tabler:home" className="text-2xl" />
+                <Icon icon="logos:linkedin" className="text-xl" />
+                <Icon icon="logos:github" className="text-xl" />
+              </div>
             </div>
           </section>
         </main>
