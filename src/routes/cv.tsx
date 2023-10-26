b16d7d3c49c981836079668a7bf1c96d5fb28085
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-26 22:51:22 +0700

    feat: layout the cards

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -1,15 +1,29 @@
 export default function CVRoute() {
   return (
-    <div className="mx-auto max-w-5xl p-4">
-      <main className="grid grid-cols-1 md:grid-cols-3">
-        <section className="col-span-1 md:col-span-1">
-          <h1>Personal column</h1>
-        </section>
+    <div className="bg-slate-200 min-h-screen">
+      <div className="mx-auto max-w-5xl p-4">
+        <main className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <section className="col-span-1 md:col-span-1 space-y-8">
+            <div className="card">
+              <h1>Name</h1>
+            </div>
 
-        <section className="col-span-1 md:col-span-2">
-          <h1>Work Column</h1>
-        </section>
-      </main>
+            <div className="card">
+              <h1>Information</h1>
+            </div>
+
+            <div className="card">
+              <h1>Skills</h1>
+            </div>
+          </section>
+
+          <section className="col-span-1 md:col-span-2">
+            <div className="card">
+              <h1>About me</h1>
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
